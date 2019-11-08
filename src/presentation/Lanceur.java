8927885fbd58f0a4508c13e683683210ceda1aa9
yoeko <lyk1719@gmail.com>
2019-11-08 16:57:07 +0100

    commit 2

--- src/presentation/Lanceur.java
+++ src/presentation/Lanceur.java
@@ -17,18 +17,24 @@ public class Lanceur {
 		List<Personne> list = new ArrayList<Personne>();
 		
 		Personne personne1 =  new Personne(2, "koffi", "komla", "kkk", "ksdjdjd", new Date(), null);
+		Personne personne2 =  new Personne(2, "kokou", "kekema", "pass", "ksdjdjd", new Date(), null);
 		Personne personne3 =  new Personne(3, "koffi", "komla", "kkk", "ksdjdjd", new Date(), null);
-		/**
-		 * test methode creerPersonne
-		 */
+				
+		//test methode creerPersonne
 		service.creerPersonne(list, personne1);
 		service.creerPersonne(list, personne3);
-		
 		System.out.println(list);
 		
-		/**
-		 * test methode getPersonne
-		 */
+		//test methode getPersonne
+//		System.out.println(service.getPersonne(list, 2));
+		
+		//test methode deletePersonne
+//		service.deletePersonne(list, 1);
+		
+		//test methode updatePersonne
+//		service.updatePersonne(list, personne2, "jean", "dubois", new Date());
+		
+		
 		
 	}
 
